9efb9749f23cae81ebb44f6792c394de7f9cf048
Joscha Feth <joscha@feth.com>
2015-09-18 22:22:44 +1000

    clean and deduplicate code

--- extension/shared/inject.js
+++ extension/shared/inject.js
@@ -5,29 +5,56 @@
 	var $body = $('body');
 	var lastTitle = "";
 
+	//Taken from http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
+	function observeDOM() {
+		var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+		var eventListenerSupported = window.addEventListener;
+
+		return function(obj, callback) {
+			if (MutationObserver) {
+				// define a new observer
+				var obs = new MutationObserver(function(mutations, observer) {
+				if (mutations[0].addedNodes.length || mutations[0].removedNodes.length)
+					callback();
+				});
+				// have the observer observe foo for changes in children
+				obs.observe(obj, { childList:true, subtree:true });
+			} else if (eventListenerSupported) {
+				obj.addEventListener('DOMNodeInserted', callback, false);
+				obj.addEventListener('DOMNodeRemoved', callback, false);
+			}
+		};
+	}
+
 	function addRating(results) {
 		//Check if hover window is available
 		var $hoverbox = $('#universal-hover');
-		if ($hoverbox.length){
-			if (isDebug) window.console.debug("Hover Div detected");
-			$box = $('#uh-starRating');
+		var $box;
+		if ($hoverbox.length) {
+			if (isDebug) {
+				window.console.debug("Hover div detected");
+			}
+			$box = $('#uh-starRating').append($('<div>', {
+				'class': 'star-rating'
+			}));
+		} else {
+			if (isDebug) {
+				window.console.debug("No hover div detected");
+			}
+			$box = $('.dp-rating-box');
 		}
-		else{
-			if (isDebug) window.console.debug("No hover Div detected");
-			var $box = $('.dp-rating-box');
-		}
-		
+
 		var hasImdbRating = $box.find('.imdb-rating').length > 0;
 
 		$.each(results, function(index, result) {
-			if(result.type === 'imdb' && hasImdbRating) {
+			if (result.type === 'imdb' && hasImdbRating) {
 				// don't add a second IMDb
 				return;
-			}			
+			}
 			var $list = $('<span>', {
 					'class': 'imdb-rating',
 					'title': result.details || ''
-				});
+			});
 			$list.append('<i class="'+result.type+'-logo-small">'+result.label+'</i>');
 			$list.append('<strong>'+result.rating+'</strong>/'+result.maxRating);
 
@@ -42,7 +69,7 @@
 
 	var annotate = function(data) {
 		var result = [];
-		if(data.imdbID && data.imdbRating !== 'N/A') {
+		if (data.imdbID && data.imdbRating !== 'N/A') {
 			result.push({
 				type: 'imdb',
 				label: 'IMDb',
@@ -51,7 +78,7 @@
 				details: null
 			});
 		}
-		if(data.tomatoMeter !== 'N/A') {
+		if (data.tomatoMeter !== 'N/A') {
 			result.push({
 				type: 'rotten',
 				label: 'Rotten Tomatoes',
@@ -65,6 +92,33 @@
 	};
 
 	var queries = [];
+	function addQueries(description, year) {
+		if (description && year) {
+			queries.push(function() {
+				return {
+					t:			description,
+					y:			year,
+					tomatoes:	true
+				};
+			});
+		}
+
+		if (description) {
+			queries.push(function() {
+				return {
+					t:			description,
+					tomatoes:	true
+				};
+			});
+		}
+		if (isDebug) {
+			window.console.log('queries:', queries.length);
+		}
+		//Get Ratings and stuff
+		if (queries.length > 0) {
+			fallbackQuery();
+		}
+	}
 
 	function getReleaseInfo() {
 		return {
@@ -78,128 +132,64 @@
 		return $.trim(title.text());
 	}
 
-	var description = getMovieDescription();
-	var year = getReleaseInfo().year;
-	if(description && year) {
-		queries.push(function() {
-			return {
-				t:			description,
-				y:			year,
-				tomatoes:	true
-			};			
-		});
-	}
-
-	if (description) {
-		queries.push(function() {
-			return {
-				t:			description,
-				tomatoes:	true
-			};
-		});
-	}
-
-	if(isDebug) {
-		window.console.log('queries:', queries.length);
-	}
-
 	function fallbackQuery() {
 		var curFn = queries.shift();
-		if(typeof curFn === 'function') {
+		if (typeof curFn === 'function') {
 			var query = curFn();
-			if(isDebug) {
+			if (isDebug) {
 				window.console.log('query', query);
 			}
 			$body.trigger('ratings.load', [query]);
 		} else {
 			window.console.error('Could not find any data');
-			var $hoverbox = $('#universal-hover');
-			var $errorText = $("<span>Could not retrieve IMDB and Rotten Tomatoes Ratings!</span>");
-			if ($hoverbox.length) $errorText.appendTo($('#uh-starRating'));
 		}
-	}	
+	}
 
 	$body.on('ratings.return', function(e, data) {
-		if(data.Response === 'True') {
-			if(isDebug) {
+		if (data.Response === 'True') {
+			queries = []; // we got a hit, so reset queries
+			if (isDebug) {
 				window.console.debug('success', data);
 			}
 			annotate(data);
 		} else {
-			if(isDebug && data.Error) {
+			if (isDebug && data.Error) {
 				window.console.debug('error', data.Error);
 			}
 			fallbackQuery(queries);
 		}
 	});
-	
 
-	//Get Ratings and stuff
-	if (queries.length > 0) {
-		fallbackQuery();
-	}
-	//Taken from http://stackoverflow.com/questions/3219758/detect-changes-in-the-dom
-	var observeDOM = (function(){
-	    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
-		eventListenerSupported = window.addEventListener;
-
-	    return function(obj, callback){
-		if( MutationObserver ){
-		    // define a new observer
-		    var obs = new MutationObserver(function(mutations, observer){
-			if( mutations[0].addedNodes.length || mutations[0].removedNodes.length )
-			    callback();
-		    });
-		    // have the observer observe foo for changes in children
-		    obs.observe( obj, { childList:true, subtree:true });
-		}
-		else if( eventListenerSupported ){
-		    obj.addEventListener('DOMNodeInserted', callback, false);
-		    obj.addEventListener('DOMNodeRemoved', callback, false);
-		}
-	    }
-	})();
+	var description = getMovieDescription();
+	var year = getReleaseInfo().year;
+	addQueries(description, year);
 
 	// Observe a specific DOM element if it exists:
-	if (document.getElementById('content') != null)	observeDOM( document.getElementById('content') ,function(){ 
-		//check if hover div exists
-		var $hoverbox = $('#universal-hover');
-		if ($hoverbox == null) return;
-		
-		//Get Movie data from hover div
-		var description = $("#header a:first").text();
-		var year = $("#uh-releaseDate").text();
-		
-		if (description == lastTitle) return;
-		
-		lastTitle = description;
-		
-		if(description && year) {
-			queries.push(function() {
-				return {
-					t:			description,
-					y:			year,
-					tomatoes:	true
-				};			
-			});
+	var $content = $('#content');
+	if ($content.length) {
+		if (isDebug) {
+			window.console.log('Found content node', $content);
 		}
+		observeDOM()($content.get(0), function() {
+			if (isDebug) {
+				window.console.log('Content changed');
+			}
+			//check if hover div exists
+			var $hoverbox = $('#universal-hover');
+			if ($hoverbox.length === 0) {
+				return;
+			}
 
-		else if (description) {
-			queries.push(function() {
-				return {
-					t:			description,
-					tomatoes:	true
-				};
-			});
-		}
-	   
-		window.console.log("Requested:"+description);
+			// Get movie data from hover div
+			var description = $hoverbox.find("#uh-title").text();
+			var year = $hoverbox.find("#uh-releaseDate").text();
 
-	   
-	   //Get Ratings and stuff
-		if (queries.length > 0) {
-			fallbackQuery();
-		}
-	});
+			if (description === lastTitle) {
+				return;
+			}
+			lastTitle = description;
+			addQueries(description, year);
+		});
+	}
 
 })(window.jQuery);
