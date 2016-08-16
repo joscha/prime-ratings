411ebcf078300da54c91a7389e260c24cce7c55a
Lennart K <lennart.k@thinking-aloud.eu>
2016-08-16 09:19:56 +0200

    fix indention

--- extension/chrome/chrome.js
+++ extension/chrome/chrome.js
@@ -4,9 +4,9 @@
 	var $body = $('body');
 	$body.on('ratings.load',function(e, query) {
 		query.t = query.t
-				.replace('[dt./OV]', '')
-				.replace('[OV]', '')
-				.replace('[OV/OmU]', '');
+			.replace('[dt./OV]', '')
+			.replace('[OV]', '')
+			.replace('[OV/OmU]', '');
 
 		$.getJSON('https://www.omdbapi.com/', query, function(data) {
 			$body.trigger('ratings.return', [data]);
