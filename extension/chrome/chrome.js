e380a8e2f3c621f1cc59cad1db8894daf58abb4b
Lennart K <lennart.k@thinking-aloud.eu>
2016-08-16 09:16:11 +0200

    Filter language specific tags in the name
    This makes the extention actually find something and therfore usable again

--- extension/chrome/chrome.js
+++ extension/chrome/chrome.js
@@ -3,12 +3,12 @@
 	
 	var $body = $('body');
 	$body.on('ratings.load',function(e, query) {
-		$.getJSON('https://www.omdbapi.com/', query, function(data) {
-			query.t = query.t
+		query.t = query.t
 				.replace('[dt./OV]', '')
 				.replace('[OV]', '')
 				.replace('[OV/OmU]', '');
 
+		$.getJSON('https://www.omdbapi.com/', query, function(data) {
 			$body.trigger('ratings.return', [data]);
 		});
 	});
